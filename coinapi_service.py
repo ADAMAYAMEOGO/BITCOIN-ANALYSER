27d6acc2fdf0e073cf0fcb19e8bea828c51eb3ca
Adama YAMEOGO <yameogoa65@gmail.com>
2023-04-29 00:47:57 +0000

    reorganise code , and create get_date_intervalle, coinapi_get_exchange_rates_extended function .

--- coinapi_service.py
+++ coinapi_service.py
@@ -1,5 +1,6 @@
 from coinapi_config import BASE_URL, API_KEY
 import json, requests
+from datetime import timedelta
 
 
 headers = {"X-CoinAPI-Key" : API_KEY}
@@ -20,6 +21,42 @@ def test_response_status(value):
             print("Too many requests -- You have exceeded your API key rate limits") 
     return value
 
+def get_date_intervalle(date_start, date_end, max_days):
+    date_intervals = []
+    current_date = date_start
+
+    while current_date < date_end:
+        next_date = current_date + timedelta(days=max_days)
+        if next_date > date_end:
+            next_date = date_end
+        date_intervals.append((current_date, next_date))
+        current_date = next_date
+
+    return date_intervals
+
+
+def coinapi_get_exchange_rates(assets, time_start, time_end):
+    time_end_str = time_end.strftime('%Y-%m-%d')
+    time_start_str = time_start.strftime('%Y-%m-%d')
+    url = BASE_URL + f'/v1/exchangerate/{assets}/history?period_id=1DAY&time_start={time_start_str}T00:00:00&time_end={time_end_str}T00:00:00'
+    headers = {'X-CoinAPI-Key' : API_KEY}
+    response = requests.get(url, headers=headers)
+    if response.status_code==200:
+        print("Quota restant : ", response.headers["x-ratelimit-remaining"])
+        return json.loads(response.text)
+    else:
+        test_response_status(response.status_code)
+        return None
+
+
+def coinapi_get_exchange_rates_extended(assets, time_start, time_end):
+    date_intervals=get_date_intervalle(time_start, time_end, 100)
+    rates = []
+    if date_intervals:
+        for i in date_intervals:
+            rates+= coinapi_get_exchange_rates(assets, i[0], i[1])
+    
+    return rates
 
 def get_all_assets():
     url = BASE_URL + "/v1/assets"
@@ -34,14 +71,4 @@ def get_all_assets():
     for i in range(10):
         print(data[i]["asset_id"], ":", data[i]["name"])
 
-def coinapi_get_exchange_rates(assets, time_start, time_end):
-        
-    url = BASE_URL + f'/v1/exchangerate/{assets}/history?period_id=1DAY&time_start={time_start}T00:00:00&time_end={time_end}T00:00:00'
-    headers = {'X-CoinAPI-Key' : API_KEY}
-    response = requests.get(url, headers=headers)
-    if response.status_code==200:
-        print("Quota restant : ", response.headers["x-ratelimit-remaining"])
-        return json.loads(response.text)
-    else:
-        test_response_status(response.status_code)
-        return None
+
--- main.py
+++ main.py
@@ -1,26 +1,47 @@
-from coinapi_service import get_all_assets, coinapi_get_exchange_rates, test_response_status
+from coinapi_service import coinapi_get_exchange_rates_extended
 from datetime import date, timedelta
 import json
+from pathlib import Path
 
 
-date_today_str = date.today().strftime('%Y-%m-%d')
-date_start_str = (date.today()-timedelta(10)).strftime('%Y-%m-%d')
+date_end_str = date.today()
+date_start_str =date.today()-timedelta(150)
 assets = "BTC/EUR"
 data_filename = assets.replace("/","_")+".json"
 
-
-
+rates = coinapi_get_exchange_rates_extended(assets, date_start_str, date_end_str)
+print()
 def get_json_rates(rates_data):
     return json.dumps([{ "date":d['time_period_start'][0:10], "Value":int(d['rate_close']) } for d in rates_data])
+
 
 def save_data_to_json_file(json_data, filename):
     with open(filename, 'w') as file:
         file.write(json_data)
         file.close()
 
-rates = coinapi_get_exchange_rates(assets, date_start_str,date_today_str)
+
+def load_json_data_from_file(data_filename_to_load):
+    with open(data_filename_to_load, 'r') as file:
+        try:
+            return json.load(file)
+        except json.JSONDecodeError:
+            print("Le fichier est vide ou contient des données JSON non valides.")
+            return []
+
+if Path(data_filename).is_file():
+    #Json File exist
+    rates= load_json_data_from_file(data_filename)
 
 if rates:
-    json_data= get_json_rates(rates)
-    print(assets, "nombres de cours : ", len(rates))
-    save_data_to_json_file(json_data, data_filename)
+    save_data_date_start_str = rates[0]["date"]
+    save_data_date_end_str = rates[-1]["date"]
+    print(save_data_date_end_str, save_data_date_start_str)
+else:
+    #Json File don't exist
+    rates = coinapi_get_exchange_rates_extended(assets, date_start_str,date_end_str)
+    if rates:
+        json_data= get_json_rates(rates)
+        print(assets, "nombres de cours : ", len(rates))
+        save_data_to_json_file(json_data, data_filename)
+
